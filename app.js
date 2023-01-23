89bf0c3e654ca13184c49ace35d9fd143b420613
jacob Mbuecky <israeljacob98@gmail.com>
2023-01-23 17:16:04 +0200

    app.js  updated

--- app.js
+++ app.js
@@ -1,24 +1,62 @@
+const url ="http://localhost:3000/basic-webdev-assessment/api/todo.php/";
+
+
 function putTodo(todo) {
     // implement your code here
-    console.log("calling putTodo");
-    console.log(todo);
+
+    fetch(url,{
+        method:'PUT',
+        body: JSON.stringify({
+            id:todo.id,
+            title:todo.title,
+            description:todo.description,
+            done:todo.done
+        }),
+        header:{
+            "Content-Type":"Application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(json => drawTodos(json))
+    .catch(error => showToastMessage('Failed to update todos...'));
+
 }
 
 function postTodo(todo) {
     // implement your code here
-    console.log("calling postTodo");
-    console.log(todo);
+    
+    fetch(url,{
+        method:'POST',
+        body: JSON.stringify({
+            id:todo.id,
+            title:todo.title,
+            description:todo.description,
+            done:todo.done
+        }),
+        header:{
+            "Content-Type":"Application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(json => drawTodos(json))
+    .catch(error => showToastMessage('Failed to create todos...'));
+   
 }
 
 function deleteTodo(todo) {
     // implement your code here
-    console.log("calling deleteTodo");
-    console.log(todo);
+    fetch(url + todo.id, {
+        method: 'DELETE',
+      })
+      .then(response => response.json())
+      .then(json => drawTodos(json))
+      .catch(error => showToastMessage('Failed to delete todos...'));
+   
 }
 
 // example using the FETCH API to do a GET request
 function getTodos() {
-    fetch(window.location.href + 'api/todo')
+    fetch(url)
     .then(response => response.json())
     .then(json => drawTodos(json))
     .catch(error => showToastMessage('Failed to retrieve todos...'));
